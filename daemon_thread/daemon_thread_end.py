d7cdfa536fdc3ea5015a7c70894ee66282267b67
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-20 23:16:59 +0530

    show the concept of daemon thread

---
+++ daemon_thread/daemon_thread_end.py
@@ -0,0 +1,24 @@
+'''
+In this code, the cleaner thread(background thread) keeps cleaning the kitchen
+even after chef is done cooking.
+'''
+import threading
+import time
+
+def infinite_thread():
+    while True:
+        print('Cleaning the kitchen')
+        time.sleep(1)
+
+if __name__ == '__main__':
+    cleaner = threading.Thread(target=infinite_thread)
+    cleaner.daemon = True
+    cleaner.start()
+
+    print('Chef is cooking')
+    time.sleep(0.5)
+
+    print('Chef is cooking')
+    time.sleep(0.7)
+
+    print('Chef is done cooking')
